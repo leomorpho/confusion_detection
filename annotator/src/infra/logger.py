8e0544e2a112660b56d1a4fd26bccfec512fbd71
leonard audibert <leonard.audibert@gmail.com>
2020-07-17 16:14:51 -0700

    Handle missing image for one video
    If a video has less frames than another, it won't crash the program. The
    missing images will use a placeholder "image missing"

--- annotator/src/infra/logger.py
+++ annotator/src/infra/logger.py
@@ -7,6 +7,6 @@ log.setLevel(logging.DEBUG)
 handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        '%(filename)s:%(lineno)d %(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 log.addHandler(handler)
