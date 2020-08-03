12db1695a49283f1921244a2a643264e0bbd64bc
Leo Audibert <leonard.audibert@gmail.com>
2020-08-03 08:52:14 -0700

    Skip already processed and remove original images of those
    The script can now re-run over already processed dirs without screwing
    up

--- create_video_dataset.py
+++ create_video_dataset.py
@@ -72,51 +72,52 @@ if __name__ == "__main__":
 
             # Skip already rendered images
             if "rendered" in filepath:
-                print("Image already rendered. Not re-processing.")
+                print("Image already rendered. Not re-rendering.")
                 continue
 
             # Skip image if already processed in earlier run
             name = ".".join(filepath.split("/")).split(".")[-2]
             if name in already_finished:
-                print("Image already processed. Not re-processing.")
+                print("Image already processed. Not re-processing. Deleting original image {filepath}")
+                os.remove(filepath)
                 continue
 
-        # # Hack to get OpenPose to work. Since there are too many images
-        # # in my directories, OpenPose crashes. Move each image to a directory
-        # # and then delete it. The results will be saved in the correct dir.
-        #     if os.path.exists(TMP):
-        #         shutil.rmtree(TMP)
+        # Hack to get OpenPose to work. Since there are too many images
+        # in my directories, OpenPose crashes. Move each image to a directory
+        # and then delete it. The results will be saved in the correct dir.
+            if os.path.exists(TMP):
+                shutil.rmtree(TMP)
 
-        #     os.mkdir(TMP)
-        #     image_name = filepath.split("/")[-1]
+            os.mkdir(TMP)
+            image_name = filepath.split("/")[-1]
 
-        #     # Copy image to TMP directory for OpenPose to process it
-        #     shutil.copyfile(filepath, f"{TMP}/{image_name}")
-        #     print(f"image_name: {image_name}")
+            # Copy image to TMP directory for OpenPose to process it
+            shutil.copyfile(filepath, f"{TMP}/{image_name}")
+            print(f"image_name: {image_name}")
 
-        #     # Run OpenPose against the TMP directory
-        #     command = f"{OPENPOSE}/build/examples/openpose/openpose.bin --image_dir {TMP} --write_images {TMP} --write_json {TMP} --display 0 -number_people_max 1"
+            # Run OpenPose against the TMP directory
+            command = f"{OPENPOSE}/build/examples/openpose/openpose.bin --image_dir {TMP} --write_images {TMP} --write_json {TMP} --display 0 -number_people_max 1"
 
-        #     if "microsoft" in platform.uname()[3].lower():
-        #         command = f"{OPENPOSE_WSL_WINDOWS} --image_dir {TMP} --write_images {TMP} --write_json {TMP} --display 0 -number_people_max 1"
-        #         print(command)
+            if "microsoft" in platform.uname()[3].lower():
+                command = f"{OPENPOSE_WSL_WINDOWS} --image_dir {TMP} --write_images {TMP} --write_json {TMP} --display 0 -number_people_max 1"
+                print(command)
 
-        #     try:
-        #         print(f"Running OpenPose for {filepath.split('/')[-1]}")
-        #         subprocess.check_call(shlex.split(command))
-        #     except subprocess.CalledProcessError as e:
-        #         print(f"Failed to run OpenPose: {e}")
+            try:
+                print(f"Running OpenPose for {filepath.split('/')[-1]}")
+                subprocess.check_call(shlex.split(command))
+            except subprocess.CalledProcessError as e:
+                print(f"Failed to run OpenPose: {e}")
 
-        #     json_file = glob.glob(f"{TMP}/*.json")[0]
-        #     image_file = glob.glob(f"{TMP}/*.png")[0]
+            json_file = glob.glob(f"{TMP}/*.json")[0]
+            image_file = glob.glob(f"{TMP}/*.png")[0]
 
-        #     # Copy rendered image back to data directory
-        #     shutil.copy(json_file, frames_dir)
-        #     picture = Image.open(image_file)
-        #     image_name_no_extension = image_name.split(".")[0]
-        #     picture.save(f"{frames_dir}/{image_name_no_extension}_rendered.jpeg", 'JPEG', optimize=True, quality=10)
-        #     # Remove original if successful
-    #   #      os.remove(filepath)
-        #     print("Success")
+            # Copy rendered image back to data directory
+            shutil.copy(json_file, frames_dir)
+            picture = Image.open(image_file)
+            image_name_no_extension = image_name.split(".")[0]
+            picture.save(f"{frames_dir}/{image_name_no_extension}_rendered.jpeg", 'JPEG', optimize=True, quality=10)
+            # Remove original if successful
+            os.remove(filepath)
+            print("Success")
 
     print(len(videos))
