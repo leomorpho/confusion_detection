fd5972eb01b3c791e891642d29a38c2b06e42ff8
sbarer <sbarer@sfu.ca>
2020-08-04 22:41:28 -0700

    fixed parsing error in csvAggregator

--- csvAggregator.py
+++ csvAggregator.py
@@ -21,6 +21,7 @@ for route in routesForRaw:
     globRoutes = glob(dirRoute)
     openFaceJsonRoutes.append(sorted(globRoutes))
 
+
 annotatedJson = []
 # import annotatedJsons
 for route in annotatedJsonRoutes:
@@ -28,6 +29,7 @@ for route in annotatedJsonRoutes:
     del data['annotator']
     del data['framesDirectory']
     annotatedJson.append(data)
+    break
 
 npAnnotated = np.asarray(annotatedJson)
 openFaceJson = []
@@ -40,7 +42,8 @@ for file in openFaceJsonRoutes:
         dataString = data.to_string(header=None, index=None)
         dataString1 = dataString.strip("{'person_id': [-1], 'pose_keypoints_2d':")
         dataString2 = dataString1.strip("], 'face_keypoints_2d': [], 'hand_left_keypoints_2d': [], 'hand_right_keypoints_2d': [], 'pose_keypoints_3d': [], 'face_keypoints_3d': [], 'hand_left_keypoints_3d': [], 'hand_right_keypoints_3d': []}")
-        stringArray = re.sub("[^\w]", " ",  dataString2).split()
+        # stringArray = re.sub("[^\w]", " ",  dataString2).split()
+        stringArray = dataString2.split()
         files.append(stringArray)
     openFaceJson.append(files)
 
@@ -55,8 +58,8 @@ for file in range(len(npAnnotated)):
         array.append(joinedRow)
     combinedArray.append(array)
 
-# for row in range(len(combinedArray[0])):
-#     print(combinedArray[0][row])
+for row in range(len(combinedArray[0])):
+    print(combinedArray[0][row])
 
 # export to json
 for i in range(len(combinedArray)):
@@ -64,5 +67,3 @@ for i in range(len(combinedArray)):
     print(route)
     with open(route, 'w') as f:
         json.dump(combinedArray, f)
-
-
