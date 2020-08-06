eb38d41138dd390adea561b7652b75ff69d4bd91
leonard audibert <leonard.audibert@gmail.com>
2020-08-05 18:06:52 -0700

    Update tests for annotator tool

--- annotator/tests/test_processor.py
+++ annotator/tests/test_processor.py
@@ -12,20 +12,20 @@ TESTDATA = "testdata"
 def test_frame_processor_creation():
     TESTDIR = f"{TESTDATA}/simplecase"
     fp = FrameProcessor(TESTDIR)
-    assert(set(fp.queue) == set(['testdata/simplecase/raw/C', 'testdata/simplecase/raw/A']))
-    assert(fp.curr_dir == f'{TESTDIR}/raw/B')
+    assert(set(fp.queue) == set(['testdata/simplecase/new_raw/C', 'testdata/simplecase/new_raw/A']))
+    assert(fp.curr_dir == f'{TESTDIR}/new_raw/B')
     assert(fp.curr_frame == 1)
 
     # Go all the way to the end of the frames
-    assert(fp.next() == "testdata/simplecase/raw/B/0001_rendered.jpeg")
+    assert(fp.next() == "testdata/simplecase/new_raw/B/0001_rendered.jpeg")
 
-    assert(fp.next() == "testdata/simplecase/raw/B/0002_rendered.jpeg")
+    assert(fp.next() == "testdata/simplecase/new_raw/B/0002_rendered.jpeg")
 
-    assert(fp.next() == "testdata/simplecase/raw/B/0003_rendered.jpeg")
+    assert(fp.next() == "testdata/simplecase/new_raw/B/0003_rendered.jpeg")
 
-    assert(fp.next() == "testdata/simplecase/raw/B/0004_rendered.jpeg")
+    assert(fp.next() == "testdata/simplecase/new_raw/B/0004_rendered.jpeg")
 
-    assert(fp.next() == "testdata/simplecase/raw/B/0005_rendered.jpeg")
+    assert(fp.next() == "testdata/simplecase/new_raw/B/0005_rendered.jpeg")
 
     # next should be idempotent
     assert(fp.next() == None)
@@ -33,15 +33,15 @@ def test_frame_processor_creation():
     assert(fp.next() == None)
 
     # Go back all the way to the birth of Jesus
-    assert(fp.prev() == "testdata/simplecase/raw/B/0005_rendered.jpeg")
+    assert(fp.prev() == "testdata/simplecase/new_raw/B/0005_rendered.jpeg")
 
-    assert(fp.prev() == "testdata/simplecase/raw/B/0004_rendered.jpeg")
+    assert(fp.prev() == "testdata/simplecase/new_raw/B/0004_rendered.jpeg")
 
-    assert(fp.prev() == "testdata/simplecase/raw/B/0003_rendered.jpeg")
+    assert(fp.prev() == "testdata/simplecase/new_raw/B/0003_rendered.jpeg")
 
-    assert(fp.prev() == "testdata/simplecase/raw/B/0002_rendered.jpeg")
+    assert(fp.prev() == "testdata/simplecase/new_raw/B/0002_rendered.jpeg")
 
-    assert(fp.prev() == "testdata/simplecase/raw/B/0001_rendered.jpeg")
+    assert(fp.prev() == "testdata/simplecase/new_raw/B/0001_rendered.jpeg")
 
 
     # Should be idempotent
@@ -49,23 +49,23 @@ def test_frame_processor_creation():
     assert(fp.prev() == None)
     assert(fp.prev() == None)
 
-    assert(fp.next() == "testdata/simplecase/raw/B/0001_rendered.jpeg")
+    assert(fp.next() == "testdata/simplecase/new_raw/B/0001_rendered.jpeg")
 
 
 def test_next_directory():
     TESTDIR = f"{TESTDATA}/simplecase"
     fp = FrameProcessor(TESTDIR)
-    assert(fp.curr_dir == "testdata/simplecase/raw/B")
+    assert(fp.curr_dir == "testdata/simplecase/new_raw/B")
     assert(fp.curr_frame == 1)
 
-    fp.next_directory(extract_frames=False)
-    assert(fp.curr_dir == "testdata/simplecase/raw/C")
+    fp.next_directory()
+    assert(fp.curr_dir == "testdata/simplecase/new_raw/C")
     assert(fp.curr_frame == 1)
 
-    fp.next_directory(extract_frames=False)
-    assert(fp.curr_dir == "testdata/simplecase/raw/A")
+    fp.next_directory()
+    assert(fp.curr_dir == "testdata/simplecase/new_raw/A")
     assert(fp.curr_frame == 1)
 
-    fp.next_directory(extract_frames=False)
+    fp.next_directory()
     assert(fp.curr_dir == None)
     assert(fp.curr_frame == 1)
